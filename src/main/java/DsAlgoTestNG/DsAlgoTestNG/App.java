724ec73c6ea0cd62e024e1c465f195f78aba611a
Sowmyapenmetsa <psowmyavarma77@gmail.com>
2025-01-16 23:21:16 -0600

    Revert "deleted default files"

---
+++ src/main/java/DsAlgoTestNG/DsAlgoTestNG/App.java
@@ -0,0 +1,10 @@
+package DsAlgoTestNG.DsAlgoTestNG;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
