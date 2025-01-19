a5313bb79b9c2e332a16cf430a71acbb597c8330
blossomnell <blossom83@gmail.com>
2025-01-18 19:11:42 -0600

    listener

--- src/test/java/Utilities/Listener.java
+++ src/test/java/Utilities/Listener.java
@@ -1,5 +1,66 @@
 package Utilities;
 
-public class Listener {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
+import baseTest.BaseTest;
+
+public class Listener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started: " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        try (FileWriter writer = new FileWriter("test-results.log", true)) {
+            writer.write("Test Passed: " + result.getName() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed: " + result.getName());
+        
+        // Take a screenshot on failure
+        WebDriver driver = BaseTest.getDriver(); // Assuming BaseTest provides WebDriver access
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshot, new File("screenshots/" + result.getName() + ".png"));
+            System.out.println("Screenshot saved for test: " + result.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped: " + result.getName());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        System.out.println("Test Failed but within success percentage: " + result.getName());
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Test Suite Started: " + context.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Test Suite Finished: " + context.getName());
+    }
 }
--- src/test/java/Utilities/RetryAnalyzer.java
+++ src/test/java/Utilities/RetryAnalyzer.java
@@ -4,17 +4,16 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
-	private int count = 0;
-    private static final int MAX_RETRY_COUNT = 3; // Set the number of retries you want
+    private int count = 0;
+    private static final int MAX_RETRY_COUNT = 2; // Retry up to 2 times
 
     @Override
     public boolean retry(ITestResult result) {
         if (count < MAX_RETRY_COUNT) {
             count++;
-            System.out.println("Retrying test: " + result.getName() + " (Attempt " + count + ")");
-            return true; // Retry the test
+            System.out.println("Retrying test: " + result.getName() + " | Attempt: " + (count + 1));
+            return true;
         }
-        return false; // No more retries
+        return false;
     }
 }
-
