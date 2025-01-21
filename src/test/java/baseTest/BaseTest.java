7a8d240777c63667120ba4d519a48ddd12f26a74
Sowmyapenmetsa <psowmyavarma77@gmail.com>
2025-01-21 13:28:12 -0600

    Changes for parallel and crossbrowser

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,6 +1,7 @@
 package baseTest;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -15,37 +16,67 @@ import Utilities.configReader;
 import driverManager.DriverManager;
 import pageObjects.LoginPage;
 	
-	public class BaseTest {    
-		protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-	        
-	  @BeforeClass(alwaysRun = true)
-	    @Parameters({"browser", "headless"})
-	    public void setUp(String browser, String headless) {
-	        boolean isHeadless = Boolean.parseBoolean(headless); 
-	        System.out.println("Initializing WebDriver for browser: " + browser + ", headless: " + isHeadless);
-	        driver.set(DriverManager.getDriver(browser, isHeadless));    
-	    }
-	    
-	    @AfterClass(alwaysRun = true)
-	    public void tearDown() {
-	    	DriverManager.quitDriver();
-//	        if (driver.get() != null) {
-//	            System.out.println("Quitting WebDriver for thread: " + Thread.currentThread().getName());
-//	            driver.get().quit();
-//	            driver.remove();
+//	public class BaseTest {    
+//		protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+//	        
+//	  @BeforeClass(alwaysRun = true)
+//	    @Parameters({"browser", "headless"})
+//	    public void setUp(String browser, String headless) {
+//	        boolean isHeadless = Boolean.parseBoolean(headless); 
+//	        System.out.println("Initializing WebDriver for browser: " + browser + ", headless: " + isHeadless);
+//	        driver.set(DriverManager.getDriver(browser, isHeadless));    
+//	    }
+//	    
+//	    @AfterClass(alwaysRun = true)
+//	    public void tearDown() {
+//	    	DriverManager.quitDriver();
+////	        if (driver.get() != null) {
+////	            System.out.println("Quitting WebDriver for thread: " + Thread.currentThread().getName());
+////	            driver.get().quit();
+////	            driver.remove();
+////	        }
+//	    }
+//	        
+//	        @AfterMethod(alwaysRun = true) 
+//	        public void clearCookies() {
+//	            if (driver != null) {
+//	                driver.get().manage().deleteAllCookies();
+//	            }
 //	        }
-	    }
-	        
-	        @AfterMethod(alwaysRun = true) 
-	        public void clearCookies() {
-	            if (driver != null) {
-	                driver.get().manage().deleteAllCookies();
-	            }
-	        }
-	    
-	    public static WebDriver getDriver() {
-	        return driver.get();
-	    }
+//	    
+//	    public static WebDriver getDriver() {
+//	        return driver.get();
+//	    }
+public class BaseTest {
+    protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"browser", "headless"})
+    public void setUp(String browser, String headless) {
+        boolean isHeadless = Boolean.parseBoolean(headless);
+        System.out.println("Setting up WebDriver for browser: " + browser + ", headless: " + isHeadless);
+        driver.set(DriverManager.getDriver(browser, isHeadless));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        if (driver.get() != null) {
+            System.out.println("Quitting WebDriver for thread: " + Thread.currentThread().getName());
+            DriverManager.quitDriver();
+        }
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void clearCookies() {
+        if (driver.get() != null) {
+            driver.get().manage().deleteAllCookies();
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
 	    
 	    public void loginToApplication() {
 	    	  try {
