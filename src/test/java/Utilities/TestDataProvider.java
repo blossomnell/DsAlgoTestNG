c4f16392bc71e4e5786ee2028cc75fc070ab4b6b
Sowmyapenmetsa <psowmyavarma77@gmail.com>
2025-01-24 13:33:55 -0600

    removed graph data from Test data

--- src/test/java/Utilities/TestDataProvider.java
+++ src/test/java/Utilities/TestDataProvider.java
@@ -32,10 +32,6 @@ public class TestDataProvider {
 	        return getExcelData("LinkedList"); 
 	    }
 	    
-	    @DataProvider(name = "PythonData")
-	    public Object[][] getExcelDataForGraph() throws IOException {
-	        return getExcelData("python DS"); 
-	    }
 	    
 	    @DataProvider(name = "ArrayData")
 	    public Object[][] getExcelDataForArray() throws IOException {
